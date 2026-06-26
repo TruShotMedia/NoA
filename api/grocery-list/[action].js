35295fbd972c2465959e51d2a1cf3a6a7fcbba11
matt herholdt <you@example.com>
2026-06-26 19:45:53 +1000

    Consolidate personalisation API into grocery route

--- api/grocery-list/[action].js
+++ api/grocery-list/[action].js
@@ -1,7 +1,10 @@
 const {
   getPublicGroceryListSummary,
+  getNoaPersonalisationSettings,
   manageGroceryItem,
   methodNotAllowed,
+  requireNoaAuth,
+  saveNoaPersonalisationSettings,
   sendJson
 } = require('../../lib/noa');
 
@@ -18,8 +21,15 @@ module.exports = async function handler(req, res) {
     return sendJson(res, 200, await manageGroceryItem(req));
   }
 
+  if (action === 'settings') {
+    if (!requireNoaAuth(req, res)) return;
+    if (req.method === 'GET') return sendJson(res, 200, await getNoaPersonalisationSettings());
+    if (req.method === 'POST') return sendJson(res, 200, await saveNoaPersonalisationSettings(req));
+    return methodNotAllowed(res);
+  }
+
   return sendJson(res, 404, {
     ok: false,
-    message: 'Unknown grocery list route. Use /api/grocery-list/summary or /api/grocery-list/item.'
+    message: 'Unknown grocery list route. Use /api/grocery-list/summary, /api/grocery-list/item, or /api/grocery-list/settings.'
   });
 };
